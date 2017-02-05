4832fb8a7ebddaaea7e6155f0f35880585917422
rocinante42 <rovertoo@gmail.com>
2017-02-04 21:31:23 -0600

    second commit

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+//import dependencies
+import React from 'react';
+//import { Link } from 'react-router';
+
+class Single extends React.Component {
+  render(){
+    return(
+      <div className="photo-grid">
+        Grid
+      </div>
+    )
+  }
+}
+
+export default Single;
---
+++ client/components/Single.js
@@ -0,0 +1,15 @@
+//import dependencies
+import React from 'react';
+//import { Link } from 'react-router';
+
+class Single extends React.Component {
+  render(){
+    return(
+      <div className="single-photo">
+        Single
+      </div>
+    )
+  }
+}
+
+export default Single;
